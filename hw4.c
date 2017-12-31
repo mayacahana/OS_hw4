906b0e11665b68163acb96af14cb268e2d111203
mayacahana <maya.cahana24@gmail.com>
2018-01-01 00:49:32 +0200

    .

--- hw4.c
+++ hw4.c
@@ -44,14 +44,19 @@ int get_size_file(int file_desc) {
     return(st.st_size);
 }
 
+void* thread_work(void* file_path) {
+    
+}
+
 int main(int argc, char** argv) {
     char* out_file = argv[1];
-    int i, active_threads;
+    int i, active_threads, rc, rc_offline, rc_cv, out_size;
     pthread_t *threads;
     int num_input_files = argc - 2;
     // init current active threads count
     active_threads = num_input_files;
     char buffer[CHUNK_SIZE], res[CHUNK_SIZE];
+    printf("Hello, creating %s from %d input files\n", out_file, num_input_files);
     int out_fd = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 00777);
     if (out_fd < 0){
         perror("ERROR: ");
@@ -59,5 +64,66 @@ int main(int argc, char** argv) {
     }
     // init the buffer
     memset(result_buffer, 0, sizeof(result_buffer));
-
+    // --- Initialize mutex  and condition variable objects -----------
+    rc = pthread_mutex_init(&buffer_mutex, NULL);
+    if (rc) {
+        perror("ERROR in pthread_mutex_init");
+        exit(-1);
+    }
+    rc_offline = pthread_mutex_init(&num_of_offline, NULL);
+    if (rc_offline) {
+        perror("ERROR in pthread_mutex_init");
+        exit(-1);
+    }
+    rc_cv = pthread_cond_init(&finish_cv, NULL);
+    if (rc_cv) {
+        perror("Error in cv init");
+        exit(-1);
+    }
+    // ----- init threads ------
+    threads = malloc(num_input_files * sizeof(pthread_t*));
+    if (!threads) {
+        perror("Error in malloc threads");
+        exit(-1);
+    }
+    // --- create reader thread for every input file ---- 
+    for (int j=2; j < argc ; j++) {
+        if (pthread_create(&threads[j-2], NULL, thread_work, argv[i]) != 0) {
+            perror("Error creating threads");
+            exit(-1);
+        }
+    }
+    // --- Wait for all threads to complete --- 
+    for (int k=0; k < num_input_files; k++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("Error joining threads");
+            exit(-1);
+        }
+    }
+    // --- check ouput file
+    out_size = get_size_file(output_fd);
+    if (out_size < 0) {
+        perror("ERROR:");
+        exit(-1);
+    }
+    if (close(output_fd) < 0) {
+        perror("ERROR:");
+        exit(-1);
+    }
+    prinf("Created %s with size %d bytes\n", out_file, out_size);
+    // --- Clean up and exit ---- 
+    free(threads);
+    if (pthread_mutex_destroy(&buffer_mutex) != 0){
+        perror("Error destoy mutex");
+        exit(-1);
+    }
+    if (pthread_mutex_destroy(&num_of_offline) != 0){
+        perror("Error destroy mutex");
+        exit(-1);
+    }
+    if (pthread_cond_destroy(&finish_cv) != 0) {
+        perror("Error destroy cv");
+        exit(-1);
+    }
+    return 0; 
 }
